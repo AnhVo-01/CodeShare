4a3ba471e3f829b85b8a4f45a2786dde265a5d17
Avin-Vo <79470360+Avin-Vo@users.noreply.github.com>
2021-10-09 22:33:36 +0700

    Add files via upload

---
+++ CSD-Assignment1-Book Management/RList/Node.java
@@ -0,0 +1,20 @@
+package RList;
+
+import LMS.Reader;
+
+public class Node {
+    Reader info;
+    Node next;
+
+    public Node() {
+    }
+
+    public Node(Reader x, Node p) {
+        info = x;
+        next = p;
+    }
+    Node(Reader x){
+        info = x;
+        next = null;
+    }
+}
