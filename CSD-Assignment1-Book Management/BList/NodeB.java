c483ae5d9201f4be2ab46f3343012727f47eab1c
Avin-Vo <79470360+Avin-Vo@users.noreply.github.com>
2021-10-09 22:32:03 +0700

    Create NodeB.java

---
+++ CSD-Assignment1-Book Management/BList/NodeB.java
@@ -0,0 +1,20 @@
+package BList;
+
+import LMS.Book;
+
+public class NodeB {
+    Book info;
+    NodeB next;
+
+    public NodeB() {
+    }
+
+    public NodeB(Book x, NodeB p) {
+        info = x;
+        next = p;
+    }
+    NodeB(Book x){
+        info = x;
+        next = null;
+    }
+}
